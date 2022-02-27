f8c38d7d8a9eea8b7a809f69b8e65db413ee80b5
genialtechie <thehaleembello@gmail.com>
2022-02-27 09:52:14 -0500

    added search tweets

--- app.js
+++ app.js
@@ -4,35 +4,57 @@ const Twitter = require('twitter');
 const fs = require('fs');
 
 const App = {
-    meme: {},
+    meme: { id: '102156234' },
+    twtObj: {
+        //tweets_ids: [],
+        text: '',
+        reply_to_id: '',
+        username: ''
+    },
     client: new Twitter({
+        //create env file with the following credentials from your twitter developer acct
         consumer_key: process.env.API_KEY,
         consumer_secret: process.env.API_KEY_SECRET,
         access_token_key: process.env.ACCESS_TOKEN,
         access_token_secret: process.env.ACCESS_TOKEN_SECRET
     }),
 
-    getMeme: async _ => {
-        try{
-            App.meme = await(await axios.get('https://api.imgflip.com/get_memes')).data.data.memes[0];
-        }
-        catch(error){
-            console.error(error);
-        }
-        return App.captionMeme();
+    searchTweets: () => {
+        App.client.get('search/tweets', { q: '#GTMBot' }, function (error, tweets, response) {
+            const tweetsArr = tweets.statuses;
+            tweetsArr.forEach(element => {
+                const found = App.twtObj.tweets_ids.includes(element.id_str);
+                console.log(found);
+
+                if (!found) {
+                    App.twtObj.text = `${element.text}`;
+                    App.twtObj.reply_to_id = `${element.id_str}`;
+                    App.twtObj.username = `${element.user.screen_name}`;
+                    App.twtObj.text = App.twtObj.text.split(' ');
+                    App.twtObj.text.pop();
+                    App.twtObj.text = App.twtObj.text.join(' ');
+                    App.twtObj.tweets_ids.push(element.id_str);
+                    
+                console.log(App.twtObj.tweets_ids)
+                    //App.captionMeme(App.twtObj.text);
+                } else return;
+            });
+        });
     },
 
-    captionMeme: async _ => {
+    captionMeme: async text => {
+        // params required to caption meme 
         const params = new URLSearchParams({
             template_id: `${App.meme.id}`,
-            username: `${process.env.IMG_USERNAME}`,
-            password: `${process.env.IMG_KEY}`,
-            text0: 'posting memes the normal way',
-            text1: 'writing a bot to post your memes'
+            username: `${process.env.IMG_USERNAME}`, // create an imgflip acct and replace 
+            password: `${process.env.IMG_KEY}`,      // these values with your own credentials in env file
+            text0: 'NOBODY:',
+            text1: `YOU: ${text}`
         });
         
         const res = await axios.post('https://api.imgflip.com/caption_image', params.toString());
         App.meme.url = `${res.data.data.url}`;
+        console.log(App.meme.url);
         return App.postMeme();
     },
 
@@ -49,24 +71,22 @@ const App = {
         const img = fs.readFileSync('./meme.jpg');
         App.client.post('media/upload', {media: img}, (error, media, response) => {
             if(!error) {
-                // If successful, a media object will be returned.
-                console.log(media);
 
                 // Lets tweet it
                 const status = {
-                status: 'I am a tweet',
+                status: `@${App.twtObj.username} #GTMBotResponse`,
+                in_reply_to_status_id: `${App.twtObj.reply_to_id}`,
                 media_ids: media.media_id_string // Pass the media id string
                 }
 
                 App.client.post('statuses/update', status, function(error, tweet, response) {
                 if (!error) {
-                    console.log(tweet);
+                    console.log('tweet was posted!');
                 }
                 });
-            }
-            console.error(error)
+            } else console.error(error);
         });
-
     }
 }
-App.getMeme();
+App.searchTweets();
+
